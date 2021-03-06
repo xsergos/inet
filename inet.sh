58291e312210eb55f3db45dd8848b5aeac2a07f2
xsergx <sergey2411999@yandex.ru>
2021-03-06 19:22:46 +0400

    0.1.5 here we go

--- inet.sh
+++ inet.sh
@@ -1,8 +1,16 @@
-# !/bin/bash
+#!/bin/bash
 
-wget https://raw.githubusercontent.com/xsergos/inet/main/e1000e.tar.gz
-tar -xvf e1000e.tar.gz
-cd e1000e/src
-make install 
-rmmod e1000e
-modprobe e1000e
+iptables -F
+if lshw -short -c network | grep 8168 2> /dev/null
+then
+	apt install r8168-dkms
+	rmmod r8169
+	modprobe r8169
+else
+	wget https://raw.githubusercontent.com/xsergos/inet/main/e1000e.tar.gz
+	tar -xvf e1000e.tar.gz
+	cd e1000e/src
+	make install 
+	rmmod e1000e
+	modprobe e1000e
+fi
