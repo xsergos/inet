08ade47c04004c5b5bfde00361d3833971c14e34
xsergos <sergey2411999@yandex.ru>
2021-03-06 21:22:18 +0400

    Update inet.sh

--- inet.sh
+++ inet.sh
@@ -5,7 +5,7 @@ if lshw -short -c network | grep 8168 2> /dev/null
 then
 	apt install r8168-dkms
 	rmmod r8169
-	modprobe r8169
+	modprobe r8168
 else
 	wget https://raw.githubusercontent.com/xsergos/inet/main/e1000e.tar.gz
 	tar -xvf e1000e.tar.gz
