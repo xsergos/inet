3172237c7d36c766801431579b7dac760ccaccfa
xsergos <sergey2411999@yandex.ru>
2021-03-06 19:43:30 +0400

    Update inet.sh

--- inet.sh
+++ inet.sh
@@ -10,7 +10,7 @@ else
 	wget https://raw.githubusercontent.com/xsergos/inet/main/e1000e.tar.gz
 	tar -xvf e1000e.tar.gz
 	cd e1000e/src
-	make install 
+	make install
 	rmmod e1000e
 	modprobe e1000e
 fi
