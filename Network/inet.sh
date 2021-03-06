85d70b1c1692118307b18b03ba3bda3671e5c096
xsergos <sergey2411999@yandex.ru>
2021-03-06 21:41:34 +0400

    Update inet.sh

--- Network/inet.sh
+++ Network/inet.sh
@@ -7,6 +7,7 @@ then
 	dpkg -i r8168.deb
 	rmmod r8169
 	modprobe r8168
+	echo "r8169" >> /etc/modprobe.d/blacklist.conf
 else
 	wget https://raw.githubusercontent.com/xsergos/inet/main/Network/e1000e.tar.gz
 	tar -xvf e1000e.tar.gz
