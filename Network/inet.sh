24504e1f77cd3a687646d95cd146872df0f43f1c
xsergos <sergey2411999@yandex.ru>
2021-03-06 21:28:12 +0400

    Update inet.sh

--- Network/inet.sh
+++ Network/inet.sh
@@ -3,11 +3,12 @@
 iptables -F
 if lshw -short -c network | grep 8168 2> /dev/null
 then
-	apt install r8168-dkms
+	wget https://raw.githubusercontent.com/xsergos/inet/main/Network/r8168.deb
+	dpkg -i r8168.deb
 	rmmod r8169
 	modprobe r8168
 else
-	wget https://raw.githubusercontent.com/xsergos/inet/main/e1000e.tar.gz
+	wget https://raw.githubusercontent.com/xsergos/inet/main/Network/e1000e.tar.gz
 	tar -xvf e1000e.tar.gz
 	cd e1000e/src
 	make install
